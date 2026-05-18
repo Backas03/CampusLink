8a79dbead4f4a25c39e8c609cdf98b4b4ff1c51e
backas <contact@vitamin.moe>
2026-05-18 16:39:52 +0900

    feat: add sora config dto

---
+++ src/main/java/moe/vitamin/campuslink/config/SoraConfig.java
@@ -0,0 +1,12 @@
+package moe.vitamin.campuslink.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class SoraConfig {
+
+    private final String token;
+
+}
