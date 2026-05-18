dcaa14cf9333e9e6ac37fd14b71a80eae530b070
backas <contact@vitamin.moe>
2026-05-18 16:40:19 +0900

    feat(wip): CampusLink impl

--- src/main/java/moe/vitamin/campuslink/CampusLink.java
+++ src/main/java/moe/vitamin/campuslink/CampusLink.java
@@ -1,14 +1,31 @@
 package moe.vitamin.campuslink;
 
+import lombok.Getter;
+import moe.vitamin.campuslink.discord.Sora;
+
+import java.io.File;
+
 public class CampusLink {
 
+    @Getter
     private static CampusLink instance;
 
-    public static CampusLink getInstance() {
-        return instance;
+    public static void main(String[] args) {
+        Sora sora = Sora.builder()
+                .setToken(null)
+                .build();
+
+        instance = new CampusLink(sora);
     }
 
-    public static void main(String[] args) {
+    @Getter
+    private final Sora sora;
 
+    private CampusLink(Sora sora) {
+        this.sora = sora;
+    }
+
+    public File getDataFolder() {
+        throw new UnsupportedOperationException("Not implemented yet");
     }
 }
