23f1cfa9fa8cc0078ae5c68d6d9d4fdc87590fcf
backas <contact@vitamin.moe>
2026-05-19 01:55:01 +0900

    feat: add snakeyaml dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ dependencies {
     implementation("net.dv8tion:JDA:6.4.1")
     implementation("org.jooq:jooq:3.19.8")
     implementation("com.zaxxer:HikariCP:6.3.3")
+    implementation("org.yaml:snakeyaml:2.2")
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
