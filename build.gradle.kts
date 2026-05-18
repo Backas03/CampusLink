1d3a63302989edd72d14bb27daf5d408045da25c
backas <contact@vitamin.moe>
2026-05-18 16:39:09 +0900

    feat: add lombok, hikari-cp, jda library

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("io.freefair.lombok") version "8.11"
 }
 
 group = "moe.vitamin.campuslink"
@@ -10,14 +11,15 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
+    compileOnly("org.projectlombok:lombok:1.18.34")
 
     implementation("net.dv8tion:JDA:6.4.1")
     implementation("org.jooq:jooq:3.19.8")
     implementation("com.zaxxer:HikariCP:6.3.3")
+
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
