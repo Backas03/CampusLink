f630292b36f9dacc5ad45dc83bc8bbb123fad895
backas <contact@vitamin.moe>
2026-05-19 01:56:51 +0900

    feat: impl database config dto

--- src/main/java/moe/vitamin/campuslink/config/DatabaseConfig.java
+++ src/main/java/moe/vitamin/campuslink/config/DatabaseConfig.java
@@ -1,4 +1,42 @@
 package moe.vitamin.campuslink.config;
 
-public class DatabaseConfig {
+import lombok.Getter;
+
+import java.io.File;
+
+@Getter
+public class DatabaseConfig extends YamlConfig {
+
+    private String driverClassName;
+    private String jdbcUrl;
+    private String username;
+    private String password;
+    private int maximumPoolSize;
+    private int minimumIdle;
+    private long maximumLifetime;
+    private long connectionTimeout;
+    private long idleTimeout;
+    private String poolName;
+
+    public DatabaseConfig(File file) {
+        super(file);
+        load();
+    }
+
+    @Override
+    public void load() {
+        super.load();
+
+        YamlNode databaseNode = getNode("database");
+        this.driverClassName = databaseNode.getString("driver-class-name");
+        this.jdbcUrl = databaseNode.getString("url");
+        this.username = databaseNode.getString("username");
+        this.password = databaseNode.getString("password");
+        this.maximumPoolSize = databaseNode.getIntOrDefault("maximum-pool-size", 10);
+        this.minimumIdle = databaseNode.getIntOrDefault("minimum-idle", 2);
+        this.maximumLifetime = databaseNode.getLongOrDefault("maximum-lifetime", 1800000L);
+        this.connectionTimeout = databaseNode.getLongOrDefault("connection-timeout", 30000L);
+        this.idleTimeout = databaseNode.getLongOrDefault("idle-timeout", 600000L);
+        this.poolName = databaseNode.getStringOrDefault("pool-name", "CampusLink-HikariPool");
+    }
 }
