ed68a23d488b02d032f7971f7e33454b06582220
backas <contact@vitamin.moe>
2026-05-18 16:40:03 +0900

    feat: add sora & sora builder

--- src/main/java/moe/vitamin/campuslink/discord/Sora.java
+++ src/main/java/moe/vitamin/campuslink/discord/Sora.java
@@ -4,6 +4,19 @@ import net.dv8tion.jda.api.JDA;
 
 public class Sora {
 
+    public static SoraBuilder builder() {
+        return new SoraBuilder();
+    }
+
+
     private final JDA jda;
+
+    protected Sora(JDA jda) {
+        this.jda = jda;
+    }
+
+    public JDA getJDA() {
+        return jda;
+    }
 
 }
--- src/main/java/moe/vitamin/campuslink/discord/SoraBuilder.java
+++ src/main/java/moe/vitamin/campuslink/discord/SoraBuilder.java
@@ -1,5 +1,43 @@
 package moe.vitamin.campuslink.discord;
 
+import moe.vitamin.campuslink.config.SoraConfig;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.ChunkingFilter;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
+
 public class SoraBuilder {
+
+    private String token;
+    private SoraConfig config;
+
+    public SoraBuilder setToken(String token) {
+        this.token = token;
+        return this;
+    }
+
+    public SoraBuilder setToLoadFromConfig(SoraConfig config) {
+        this.config = config;
+        return this;
+    }
+
+    public Sora build() {
+        JDABuilder builder;
+        if (config != null) {
+            builder = JDABuilder.createDefault(config.getToken());
+        } else {
+            builder = JDABuilder.createDefault(token);
+        }
+        JDA jda = builder
+                .setChunkingFilter(ChunkingFilter.ALL)
+                .setMemberCachePolicy(MemberCachePolicy.ALL)
+                .enableIntents(GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MEMBERS)
+                .enableCache(CacheFlag.ROLE_TAGS)
+                .build();
+
+        return new Sora(jda);
+    }
 
 }
