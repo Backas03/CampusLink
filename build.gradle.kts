f9618bcd4cbed3ef056eceba3ee5263df6d708ac
backas <contact@vitamin.moe>
2026-05-18 16:23:39 +0900

    feat: add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,11 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+
+    implementation("net.dv8tion:JDA:6.4.1")
+    implementation("org.jooq:jooq:3.19.8")
+    implementation("com.zaxxer:HikariCP:6.3.3")
 }
 
 tasks.test {
