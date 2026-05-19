16d41acf8ad3771e659c7ee58d825d8f4a0143a1
backas <contact@vitamin.moe>
2026-05-19 14:24:40 +0900

    feat: refactor SoraBuilder to require SoraConfig for building Sora instances

--- src/main/java/moe/vitamin/campuslink/discord/SoraBuilder.java
+++ src/main/java/moe/vitamin/campuslink/discord/SoraBuilder.java
@@ -10,34 +10,25 @@ import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
 public class SoraBuilder {
 
-    private String token;
     private SoraConfig config;
 
-    public SoraBuilder setToken(String token) {
-        this.token = token;
-        return this;
-    }
-
-    public SoraBuilder setToLoadFromConfig(SoraConfig config) {
+    public SoraBuilder setConfig(SoraConfig config) {
         this.config = config;
         return this;
     }
 
     public Sora build() {
-        JDABuilder builder;
-        if (config != null) {
-            builder = JDABuilder.createDefault(config.getToken());
-        } else {
-            builder = JDABuilder.createDefault(token);
+        if (config == null) {
+            throw new IllegalStateException("Config must be set");
         }
-        JDA jda = builder
+        JDA jda = JDABuilder.createDefault(config.getToken())
                 .setChunkingFilter(ChunkingFilter.ALL)
                 .setMemberCachePolicy(MemberCachePolicy.ALL)
                 .enableIntents(GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MEMBERS)
                 .enableCache(CacheFlag.ROLE_TAGS)
                 .build();
 
-        return new Sora(jda);
+        return new Sora(jda, config);
     }
 
 }
