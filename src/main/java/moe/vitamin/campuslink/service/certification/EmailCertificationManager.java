cc4014dfd65e3aac2814f9522dcbc6a4f12de41c
Backas03 <71801733+Backas03@users.noreply.github.com>
2026-05-20 17:32:20 +0900

    feat(wip): email certification impl

--- src/main/java/moe/vitamin/campuslink/service/certification/EmailCertificationManager.java
+++ src/main/java/moe/vitamin/campuslink/service/certification/EmailCertificationManager.java
@@ -1,6 +1,12 @@
 package moe.vitamin.campuslink.service.certification;
 
 import moe.vitamin.campuslink.service.certification.database.EmailCertificationDao;
+import moe.vitamin.campuslink.service.certification.result.EmailCertificationRequestResult;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.regex.Pattern;
 
 public class EmailCertificationManager {
 
@@ -10,9 +16,30 @@ public class EmailCertificationManager {
         return new EmailCertificationManager();
     }
 
+    private final Pattern emailPattern;
+
     private EmailCertificationManager() {
+        this.emailPattern = Pattern.compile("^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@daegu\\.ac\\.kr");
+    }
+
+
+    public CompletableFuture<EmailCertificationRequestResult> requestCertification(
+            String email,
+            User user
+    ) {
+        if (!isValidEmail(email)) {
+            return CompletableFuture.completedFuture(EmailCertificationRequestResult.INVALID_EMAIL);
+        }
+
+        return CompletableFuture.supplyAsync(() -> {
+
+            return null;
+        });
 
     }
 
+    public boolean isValidEmail(String email) {
+        return emailPattern.matcher(email).matches();
+    }
 
 }
