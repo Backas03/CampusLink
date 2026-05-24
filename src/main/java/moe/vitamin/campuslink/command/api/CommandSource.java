eda7130f581f6af433218fd4964d390bc61f816f
backas <contact@vitamin.moe>
2026-05-24 21:04:59 +0900

    feat: add CommandSource and SlashCommandSource interfaces for command handling

---
+++ src/main/java/moe/vitamin/campuslink/command/api/CommandSource.java
@@ -0,0 +1,11 @@
+package moe.vitamin.campuslink.command.api;
+
+import org.jetbrains.annotations.Nullable;
+
+public interface CommandSource {
+
+    @Nullable String getDescription();
+
+    @Nullable String getUsage();
+
+}
