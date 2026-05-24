c11b5ac7735c1cdae887cfc8da855d0062b1a611
backas <contact@vitamin.moe>
2026-05-24 19:15:11 +0900

    feat: add EmailCertificationVerificationResult enum for certification status

---
+++ src/main/java/moe/vitamin/campuslink/service/certification/result/EmailCertificationVerificationResult.java
@@ -0,0 +1,11 @@
+package moe.vitamin.campuslink.service.certification.result;
+
+public enum EmailCertificationVerificationResult {
+
+    NOT_IN_PROGRESS,
+    INVALID_CODE,
+    EXPIRED,
+    INTERNAL_ERROR,
+    TIMEOUT,
+    SUCCESS
+}
